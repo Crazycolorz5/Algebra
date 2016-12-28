92d9e17f7d43cfa3fcd4b83b289e1394c4978128
Crazycolorz5 <crazycolorz5@gmail.com>
2016-12-28 15:51:20 -0500

    Added instance of Show.

--- BinaryTree.hs
+++ BinaryTree.hs
@@ -1,3 +1,5 @@
+module BinaryTree where
+
 import Algebra
 
 --A binary tree is the fixed point of (1 + A*X*X)
@@ -8,5 +10,17 @@ instance Functor (Node a) where
     fmap f (Node a b1 b2) = Node a (f b1) (f b2)
 
 data BinaryTree a = Tree {unTree:: Fix (Node a)}
---Now we get initial, unInitial, and catamorphism.
+--Now we get initial, unInitial, catamorphism, anamorphism, and hylomorphism.
+
+empty = Tree {unTree = initial EmptyNode}
+singleton x = Tree {unTree = initial (Node x (initial EmptyNode) (initial EmptyNode))}
+makeTree node l r = Tree {unTree = initial $ Node node (unTree l) (unTree r)}
+
+treeCata alg = catamorphism alg . unTree
+treeAna coalg = Tree . anamorphism coalg
+
+instance (Show a) => Show (BinaryTree a) where
+    show = treeCata (\n -> case n of EmptyNode -> ""; Node a l r -> show a++"("++l++","++r++")")
+
+
 
