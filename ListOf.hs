87a6fd7f7e6312dcee82ddba06fa53b7891c384b
Crazycolorz5 <crazycolorz5@gmail.com>
2016-12-28 15:50:42 -0500

    Implemented take/countup/down as anamorphisms (all run in linear time now).

--- ListOf.hs
+++ ListOf.hs
@@ -1,13 +1,13 @@
 module ListOf where
 
 import Algebra
-import Control.Arrow (first, second)
+import Control.Arrow (first, second, (>>>), (&&&))
 
-data Product a x = Unit | Prod a x --Curried 1+A*X as a type.
+data Product a x = Unit | Prod a x deriving Eq --Curried 1+A*X as a type.
 productCase :: c -> ((a, b) -> c) -> Product a b -> c --utility function in place of pattern matching
 productCase c _ Unit = c
 productCase _ f (Prod a b) = f (a,b)
-instance Functor (Product a) where --Proof that it's an endofunctor
+instance Functor (Product a) where
     fmap f = productCase Unit (\(a,b) -> Prod a (f b))
 data ListOf a = List (Fix (Product a)) --The List type is the fixed point of a functor with the signature 1 + A*X, where A is a fixed type.
 
@@ -32,13 +32,15 @@ instance (Show a) => Show (ListOf a) where
             in catamorphism g . unList
 
 listCata alg = catamorphism alg . unList
+listAna coalg = List . anamorphism coalg
             
 instance Functor ListOf where
     fmap f = listCata (productCase nil (cons . first f))
 
-takeOfList 0 = const nil
-takeOfList n = listCata (productCase nil (cons . second (takeOfList (pred n))))
-                      
+--takeOfList 0 = const nil
+--takeOfList n = listCata (productCase nil (cons . second (takeOfList (pred n))))
+takeOfList = curry $ listAna (\(n,l) -> if n == 0 then Unit else case uncons l of Nothing -> Unit; Just (h, t) -> Prod h (pred n, t))
+
 lengthOfList = listCata (productCase 0 (succ . snd))
 
 concatOfLists l1 l2 = listCata (productCase l2 cons) l1
@@ -50,8 +52,9 @@ twos = List (initial (Prod 2 (unList twos)))
 
 testList = cons(5,cons(4,cons(3,cons(2,cons(1,cons(0,nil))))))
 
-countdown 0 = cons(0, nil)
-countdown n = cons(n, countdown (pred n))
+countdown = listAna (\n -> if n < 0 then Unit else Prod n (pred n))
+countup x = listAna (\n -> if n > x then Unit else Prod n (succ n)) 0
+zeroDotDot = listAna (id &&& succ >>> uncurry Prod) 0
 
-zerodotdot = cons(0, fmap succ zerodotdot)--TODO: I can express this as an anamorphism
-countup n = takeOfList n zerodotdot --Yeah, yeah, quadratic time
+--zerodotdot = cons(0, fmap succ zerodotdot)--TODO: I can express this as an anamorphism
+--countup n = takeOfList n zerodotdot --Yeah, yeah, quadratic time
