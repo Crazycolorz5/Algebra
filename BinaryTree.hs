0dfb8c95693b38124a89357140fe0c3d38971c2d
Crazycolorz5 <crazycolorz5@gmail.com>
2017-01-04 16:41:53 -0500

    Added height and size as catamorphisms, tree generation as anamorphisms.

--- BinaryTree.hs
+++ BinaryTree.hs
@@ -19,8 +19,17 @@ makeTree node l r = Tree {unTree = initial $ Node node (unTree l) (unTree r)}
 treeCata alg = catamorphism alg . unTree
 treeAna coalg = Tree . anamorphism coalg
 
+nodeCase b _ EmptyNode = b
+nodeCase _ f (Node a b c) = f a b c
+
 instance (Show a) => Show (BinaryTree a) where
     show = treeCata (\n -> case n of EmptyNode -> ""; Node a l r -> show a++"("++l++","++r++")")
 
+height = treeCata (nodeCase 0 (const $ (. succ) . max))
+size = treeCata (nodeCase 0 (const $ (. succ) . (+)))
+
+preOrder n = treeAna (\(x,y) -> if x==0 then EmptyNode else Node y (x-1, y+1) (x-1, y+2^(x-1))) (n,0)
+inOrder n = treeAna (\(x,y) -> if x==0 then EmptyNode else Node (y+2^(x-1)) (x-1, y) (x-1, y+2^(x-1))) (n,0)
 
 
+testTree = makeTree "e" (makeTree "L" (singleton "LL") (singleton "LR")) (singleton "R")
